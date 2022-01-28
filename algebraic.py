9c003cff294eb1459be215da1e1ad56fc7c7ef02
pedro <p.ribeiro.engenheiro@gmail.com>
2022-01-28 15:05:01 +0000

    Finished V1. First stage works, second stage doesn't because cannot access dict. maybe we need to change the way we access this, we'll see.

--- algebraic.py
+++ algebraic.py
@@ -1,18 +1,39 @@
 import random
+import time
+from threading import Timer
 from dataclasses import dataclass, field
+
+
 # TODO: need to add timer
 # TODO: need to add storage class
 # TODO: need to add second stage band finish first stage
 
+
 @dataclass
-class Algebraic:
+class Storage:
+    listing: list
+
+    def open_list(self):
+        self.listing = []
+
+    def dict_maker(self, text: str, result: float, timer: float):
+        dictionary = {
+            'operation': text,
+            'result': result,
+            'time': time
+        }
+        self.listing.append(dictionary)
+
+
+@dataclass
+class Algebraic(Storage):
     """
     Class manages the creation of algebraic operations for user completion.
     """
-    x: int
-    y: int
-    z: int
-    b: int
+    x: int = 1
+    y: int = 1
+    z: int = 1
+    b: int = 1
     allowed_types: list = field(default_factory=lambda: ['sum', 'sub', 'divide'])
 
     def type_definition(self, count, dx, dy, dz, db):
@@ -47,13 +68,11 @@ class Algebraic:
         :param db:
         :return:
         """
-        if dx is int and dy is int and dz is int and db is int:
-            self.x = dx
-            self.y = dy
-            self.z = dz
-            self.b = db
-        else:
-            raise ValueError("all status must be integers")
+
+        self.x = dx
+        self.y = dy
+        self.z = dz
+        self.b = db
 
     def sum(self):
         """
@@ -87,10 +106,10 @@ class Algebraic:
         randomizes the process of creating the algebraic operations.
         :return:
         """
-        dx = random.randrange(1, 51)
-        dy = random.randrange(1, 51)
-        dz = random.randrange(1, 51)
-        db = random.randrange(1, 51)
+        dx = random.randrange(1, 10)
+        dy = random.randrange(1, 10)
+        dz = random.randrange(1, 10)
+        db = random.randrange(1, 10)
         count = random.choice(self.allowed_types)
         math, text = self.type_definition(count, dx, dy, dz, db)
         return math, text
@@ -101,11 +120,12 @@ class Algebraic:
         :return:
         """
         math, text = self.make_algebra()
-        response = int(input(text))
+        response = float(input(text))
         if response == math:
             print('Correct')
         else:
             print('Wrong')
+        return math, text
 
     def first_phase(self):
         """
@@ -115,7 +135,30 @@ class Algebraic:
         start_text = 'You will now be shown several algebraic operations. You will have to solve each of them. \n' \
                      'Take as long as you need to perform this task. You must not use paper, or a calculator. \n'
         print(start_text)
-        for
-        self.count_control()
+        time.sleep(5)
+        for i in range(1, 10):
+            start_time = time.time() * 1000
+            math, text = self.count_control()
+            final_time = time.time() * 1000
+            timer = final_time - start_time
+            self.dict_maker(text, math, timer)
+        self.second_stage()
 
-    def second_stage
+    def second_stage(self):
+        start_text = 'You will now be shown the same operations you completed earlier. \n ' \
+                     'You will have a certain amount of time to complete these operations again. \n' \
+                     'This time will be half the time you took to complete them the first time.'
+        print(start_text)
+        for i in self.listing:
+            operation = self.listing[i]['operation']
+            result = self.listing[i]['result']
+            timeout = self.listing[i]['timer']
+            t = Timer(timeout, print, ['Sorry, times up'])
+            t.start()
+            response = input(operation)
+            t.cancel()
+            if response == result:
+                print('Correct!')
+            else:
+                print('Wrong!')
+            print('Thank you.')
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 from algebraic import Algebraic
+
+alg = Algebraic(['sum', 'sub', 'divide'], 1, 2, 3, 4)
+
+alg.open_list()
+alg.first_phase()
