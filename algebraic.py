f4ed247df137256de0663b408792b5b1cf8750d6
pedro <p.ribeiro.engenheiro@gmail.com>
2022-01-28 13:59:21 +0000

    added documentation

--- algebraic.py
+++ algebraic.py
@@ -1,12 +1,14 @@
 import random
 from dataclasses import dataclass, field
-# TODO: need to add documentation
 # TODO: need to add timer
 # TODO: need to add storage class
 # TODO: need to add second stage band finish first stage
 
 @dataclass
 class Algebraic:
+    """
+    Class manages the creation of algebraic operations for user completion.
+    """
     x: int
     y: int
     z: int
@@ -14,6 +16,16 @@ class Algebraic:
     allowed_types: list = field(default_factory=lambda: ['sum', 'sub', 'divide'])
 
     def type_definition(self, count, dx, dy, dz, db):
+        """
+        Type definition checks if type is allowed type and feeds paremeters count, dx, dy, dz, db to the specific
+        type functions.
+        :param count:
+        :param dx:
+        :param dy:
+        :param dz:
+        :param db:
+        :return:
+        """
         if count in self.allowed_types:
             self.make_xyz(dx, dy, dz, db)
             if count == 'sum':
@@ -27,6 +39,14 @@ class Algebraic:
         return math, text
 
     def make_xyz(self, dx: int, dy: int, dz: int, db: int):
+        """
+        Turns all operational parameters into class parameters.
+        :param dx:
+        :param dy:
+        :param dz:
+        :param db:
+        :return:
+        """
         if dx is int and dy is int and dz is int and db is int:
             self.x = dx
             self.y = dy
@@ -36,21 +56,37 @@ class Algebraic:
             raise ValueError("all status must be integers")
 
     def sum(self):
+        """
+        Deals with sum type operations.
+        :return:
+        """
         result = (self.b - self.y) / self.z
         text = f'{self.z}x + {self.y} = {self.b} \n'
         return result, text
 
     def sub(self):
+        """
+        Deal with sub type operations.
+        :return:
+        """
         result = (self.b + self.y) / self.z
         text = f'{self.z}x - {self.y} = {self.b} \n'
         return result, text
 
     def divide(self):
+        """
+        Deals with divide type operations.
+        :return:
+        """
         result = (self.b * self.y) / self.z
         text = f'{self.z}x/{self.y} = {self.b} \n'
         return result, text
 
     def make_algebra(self):
+        """
+        randomizes the process of creating the algebraic operations.
+        :return:
+        """
         dx = random.randrange(1, 51)
         dy = random.randrange(1, 51)
         dz = random.randrange(1, 51)
@@ -60,6 +96,10 @@ class Algebraic:
         return math, text
 
     def count_control(self):
+        """
+        Automates subject interaction.
+        :return:
+        """
         math, text = self.make_algebra()
         response = int(input(text))
         if response == math:
@@ -68,6 +108,10 @@ class Algebraic:
             print('Wrong')
 
     def first_phase(self):
+        """
+        Manages first algebraic game phase.
+        :return:
+        """
         start_text = 'You will now be shown several algebraic operations. You will have to solve each of them. \n' \
                      'Take as long as you need to perform this task. You must not use paper, or a calculator. \n'
         print(start_text)
